b8366cdaffba38f801451513a931156202d4cce4
Laurel <laurel2195@gmail.com>
2018-02-24 16:59:17 -0500

    i finished video 3

---
+++ Assets/PlayerSetup.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+    Camera sceneCamera;
+
+    //launched when the player spawn, because we extend the class from NetworkBehavior
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            for(int i = 0; i < componentsToDisable.Length; i++)
+            {
+                componentsToDisable[i].enabled = false;
+            }
+        } else
+        {
+            sceneCamera = Camera.main; //we can call Camera.main, because we added the 'tag' main to that camera
+            if (sceneCamera)
+            {
+                sceneCamera.gameObject.SetActive(false);
+            }
+        }  
+    }
+
+    void OnDisable()
+    {
+        if (sceneCamera)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
+}
--- Assets/player_controller.cs
+++ Assets/player_controller.cs
@@ -7,6 +7,8 @@ public class player_controller : MonoBehaviour {
 
     [SerializeField]
     private float speed = 5f;
+    [SerializeField]
+    private float lookSensitivity = 3f;
 
     private player_motor motor;
 
@@ -18,17 +20,32 @@ public class player_controller : MonoBehaviour {
     void Update()
     {
         //calculate moveiemtnv elocuty as a 3d vector
-        float _xMove = Input.GetAxisRaw("Horizontal");
-        float _zMove = Input.GetAxisRaw("Vertical");
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _zMov = Input.GetAxisRaw("Vertical");
 
-        Vector3 _moveHorizontal = transform.right * _xMove;
-        Vector3 _moveVertical = transform.forward * _zMove;
+        Vector3 _movHorizontal = transform.right * _xMov;
+        Vector3 _movVertical = transform.forward * _zMov;
 
         //final movement vector
-        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * speed;
+        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
 
         //apply movement
         motor.Move(_velocity);
 
+        //calculate rotation as a 3d vector (turning around, not up down cuz we dont want the player to go flying...)
+        float _yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0f, _yRot, 0f) * lookSensitivity;
+
+        //Apply rotation
+        motor.Rotate(_rotation);
+
+        //calculate camera rotation
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _cameraRotation = new Vector3(_xRot, 0f, 0f) * lookSensitivity;
+
+        //Apply rotation
+        motor.RotateCamera(_cameraRotation);
     }
 }
